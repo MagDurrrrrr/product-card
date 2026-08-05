a076b7991279cc17afd7543d1454e55cdb66ee55
MagDurrrrr <canadamag555@gmail.com>
2026-08-06 01:21:20 +0300

    Fix homework 9 review comments

--- scripts/homework-9.js
+++ scripts/homework-9.js
@@ -1,30 +1,32 @@
 import { comments } from "./comments.js";
+
 // Задание №2. Фильтрация от 5 и далее
 const numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
-const filteredNumbers = numbers.filter(
-    function (number) {
+
+const filteredNumbers = numbers.filter(function (number) {
   return number >= 5;
-}
-);
+});
 
 console.log(filteredNumbers);
 
 // Задание №3. Проверка наличия элемента в массиве
 const kitchenAppliances = [
-    "Блендер",
-    "Чайник",
-    "Микроволновка",
-    "Посудомойка",
-    "Вытяжка",
+  "Блендер",
+  "Чайник",
+  "Микроволновка",
+  "Посудомойка",
+  "Вытяжка",
 ];
+
 const hasToaster = kitchenAppliances.includes("Тостер");
 console.log(hasToaster);
+
 const hasKettle = kitchenAppliances.includes("Чайник");
 console.log(hasKettle);
 
 // Задание №4. Изменение порядка элементов массива на противоположный
 function reverseArray(array) {
-  return array.reverse()
+  array.reverse();
 }
 
 reverseArray(numbers);
@@ -37,26 +39,20 @@ console.log(kitchenAppliances);
 const commentsWithComEmail = comments.filter(function (comment) {
   return comment.email.includes(".com");
 });
+
 console.log(commentsWithComEmail);
 
 // Задание №8. Изменение postId
 const commentsWithNewPostId = comments.map(function (comment) {
-  if (comment.id <= 5) {
-    return {
-      ...comment,
-      postId: 2,
-    };
-  }
-
   return {
     ...comment,
-    postId: 1,  
+    postId: comment.id <= 5 ? 2 : 1,
   };
 });
 
 console.log(commentsWithNewPostId);
 
-//Задание №9. Перебор массива
+// Задание №9. Перебор массива
 const arrayWithIdAndName = comments.map(function (comment) {
   return {
     id: comment.id,
@@ -66,7 +62,7 @@ const arrayWithIdAndName = comments.map(function (comment) {
 
 console.log(arrayWithIdAndName);
 
-//Задание №10. Свойство isInvalid
+// Задание №10. Свойство isInvalid
 const validatedComments = comments.map(function (comment) {
   return {
     ...comment,
@@ -76,7 +72,7 @@ const validatedComments = comments.map(function (comment) {
 
 console.log(validatedComments);
 
-//Задание 11. reduce и map
+// Задание №11. reduce и map
 const emailsWithMap = comments.map(function (comment) {
   return comment.email;
 });
